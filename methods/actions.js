d9c028bcda770aea046bf64b80f040043a51efd5
SamridhiGusain <66431018+SamridhiGusain@users.noreply.github.com>
2021-07-09 04:04:46 +0530

    Update actions.js

--- methods/actions.js
+++ methods/actions.js
@@ -4,12 +4,14 @@ var config = require('../config/dbconfig')
 
 var functions = {
     addNew: function (req, res) {
-        if ((!req.body.name) || (!req.body.password)) {
+        if ((!req.body.email) || (!req.body.password)) {
             res.json({success: false, msg: 'Enter all fields'})
         }
         else {
             var newUser = User({
-                name: req.body.name,
+                firstName:req.body.firstName,
+                lastName:req.body.lastName,
+                email: req.body.email,
                 password: req.body.password
             });
             newUser.save(function (err, newUser) {
@@ -24,12 +26,12 @@ var functions = {
     },
     authenticate: function (req, res) {
         User.findOne({
-            name: req.body.name
+            email: req.body.email
         }, function (err, user) {
                 if (err) throw err
                 if (!user) {
                     res.status(403).send({success: false, msg: 'Authentication Failed, User not found'})
-                }
+                } 
 
                 else {
                     user.comparePassword(req.body.password, function (err, isMatch) {
@@ -49,7 +51,7 @@ var functions = {
         if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
             var token = req.headers.authorization.split(' ')[1]
             var decodedtoken = jwt.decode(token, config.secret)
-            return res.json({success: true, msg: 'Hello ' + decodedtoken.name})
+            return res.json({success: true, msg: 'Hey ' + decodedtoken.firstName + '!'})
         }
         else {
             return res.json({success: false, msg: 'No Headers'})
